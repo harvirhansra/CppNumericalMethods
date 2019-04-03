586a11311870a8681ff03530f75d2cb3f00b7337
Harvir Hansra <h.hansra97@gmail.com>
2019-04-03 22:13:37 +0100

    Renamed repo and FiniteDifferences class

---
+++ Differentiation.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <cmath>
+#include "Differentiation.h"
+
+using namespace std;
+
+Differentiation::Differentiation(double (*func)(double)) {
+   f = func;
+}
+
+double Differentiation::forward_difference(double x, double h){
+    this->method = "forward difference";
+    
+    return ((*f)(x+h) - (*f)(x)) / h;
+    
+}
+
+double Differentiation::backward_difference(double x, double h){
+    this->method = "backward difference";
+    
+    return ((*f)(x) - (*f)(x-h)) / h;
+    
+}
+
+double Differentiation::central_difference(double x, double h){
+    this->method = "central difference";
+    
+    return ((*f)(x+h) - (*f)(x-h)) / (2*h);
+    
+}
+
+double Differentiation::five_point(double x, double h){
+    this->method = "five point";
+    double first, second, third, fourth;
+    first = (*f)(x+(2*h));
+    second = (*f)(x+h);
+    third = (*f)(x-h);
+    fourth = (*f)(x-(2*h));
+    
+    return (-first + 8*second - 8*third + fourth)/(12*h);
+}
+
+double Differentiation::richardson_extrapolation(double x, double h){
+    this->method = "richardson extrapolation";
+    double first, second, third;
+    first = ((*f)(x+(h/2)));
+    second = ((*f)(x+h));
+    third = ((*f)(x));
+    
+    return (4*first - second - 3*third) / h;
+    
+}
+
---
+++ Differentiation.h
@@ -0,0 +1,21 @@
+#ifndef FINITEDIFFERENCES_H
+#define FINITEDIFFERENCES_H
+
+#include <string>
+#include <cmath>
+
+class Differentiation {
+public:
+    Differentiation(double (*func)(double));
+    double forward_difference(double x, double h);
+    double backward_difference(double x, double h);
+    double central_difference(double x, double h);
+    double five_point(double x, double h);
+    double richardson_extrapolation(double x, double h);
+    std::string method;
+private:
+    double (*f)(double);
+};
+
+#endif /* FINITEDIFFERENCES_H */
+
